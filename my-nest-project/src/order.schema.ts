cb433336d257a0dd9995fd64a1985bb8cde1cbd7
frederic <flausson@normandiewebschool.fr>
2024-10-17 10:16:11 +0200

    fix: 🐛 séparate services

--- my-nest-project/src/order.schema.ts
+++ my-nest-project/src/order.schema.ts
@@ -1,7 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { Types } from 'mongoose';
-import { Product } from './product.schema';
 
 
 export type OrderDocument = Order & Document;
@@ -9,7 +8,7 @@ export type OrderDocument = Order & Document;
 @Schema()
 export class Order {
   @Prop({ type: Types.ObjectId, ref: 'Product', required: true })
-  productId: Product;
+  productId: Types.ObjectId;
 
   @Prop({ required: true })
   quantity: number;
