cce8bcae9c13c9baa03e10875204f840ac096a2c
frederic <flausson@normandiewebschool.fr>
2024-10-17 09:44:55 +0200

    feat: ✨ products

---
+++ products/src/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product, ProductDocument } from './product.schema';
+
+@Injectable()
+export class ProductService {
+  constructor(@InjectModel(Product.name) private productModel: Model<ProductDocument>) {}
+
+  async createProduct(name: string, price: number, quantity: number): Promise<Product> {
+    const newProduct = new this.productModel({ name, price, quantity });
+    return newProduct.save();
+  }
+
+  async findAll(): Promise<Product[]> {
+    return this.productModel.find().exec();
+  }
+}
